c0f35f75a7a080ffa4ff7b5db1f0815b2579b255
Ivan Solovev <ivan@solovev.one>
2019-03-22 09:44:45 +0300

    Remove file compression in production

--- build.js
+++ build.js
@@ -1,12 +1,5 @@
-const path = require("path");
 const fs = require("fs");
 const { promisify } = require("util");
-
-const readDirRecursive = require("recursive-readdir");
-const zopfli = require('@gfx/zopfli');
-const brotli = require('iltorb');
-const pQueue = require('p-queue');
-const chalk = require('chalk');
 
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
@@ -27,27 +20,6 @@ const linksElements = [
 	"js-info-complex",
 	"js-cubic-bezier"
 ];
-
-const compressConfig = {
-	gzip: {
-		enabled: true,
-		numiterations: 15,
-		blocksplitting: true,
-		blocksplittinglast: false,
-		blocksplittingmax: 15,
-	},
-	brotli: {
-		enabled: true,
-		mode: 0,
-		quality: 11,
-		lgwin: 22,
-		lgblock: 0,
-		enable_dictionary: true,
-		enable_transforms: false,
-		greedy_block_split: false,
-		enable_context_modeling: false,
-	}
-};
 
 const shortCssClassName = generateCssClassName();
 
@@ -169,32 +141,7 @@ async function build() {
 		});
 }
 
-const queue = new pQueue({ concurrency: 2 });
-
 build()
-	.then(() => readDirRecursive(path.join(__dirname, "dist")))
-	.then(async (files) => {
-		console.log(chalk.bold('\n🗜️  Compressing bundled files...'));
-		const start = new Date().getTime();
-
-		try {
-			files
-				.filter((file) => bundleAssets.find((item) => item.name === file))
-				.forEach((file) => {
-					queue.add(() => zopfliCompress(file, compressConfig.gzip));
-					queue.add(() => brotliCompress(file, compressConfig.brotli));
-				});
-
-			await queue.onIdle();
-
-			const end = new Date().getTime();
-			console.log(chalk.bold.green(`✨  Compressed in ${((end - start) / 1000).toFixed(2)}s.\n`));
-		} catch (error) {
-			console.error(chalk.bold.red('❌  Compression error:\n'));
-			process.stderr.write(error.stack + "\n");
-			process.exit(1);
-		}
-	})
 	.catch(error => {
 		process.stderr.write(error.stack + "\n");
 		process.exit(1);
@@ -246,72 +193,4 @@ function* generateCssClassName() {
 
 		yield result;
 	}
-}
-
-function zopfliCompress(file, config) {
-	if (!config.enabled) {
-		return Promise.resolve();
-	}
-
-	const stat = fs.statSync(file);
-
-	if (!stat.isFile()) {
-		return Promise.resolve();
-	}
-
-	if (config.threshold && stat.size < config.threshold) {
-		return Promise.resolve();
-	}
-
-	return new Promise((resolve, reject) => {
-		fs.readFile(file, (error, content) => {
-			if (error) {
-				return reject(error);
-			}
-
-			zopfli.gzip(content, config, (error, compressedContent) => {
-				if (error) {
-					return reject(error);
-				}
-
-				if (stat.size > compressedContent.length) {
-					fs.writeFile(`${file}.gz`, compressedContent, () => resolve());
-				} else {
-					resolve();
-				}
-			});
-		});
-	});
-}
-
-function brotliCompress(file, config) {
-	if (!config.enabled) {
-		return Promise.resolve();
-	}
-
-	const stat = fs.statSync(file);
-
-	if (!stat.isFile()) {
-		return Promise.resolve();
-	}
-
-	if (config.threshold && stat.size < config.threshold) {
-		return Promise.resolve();
-	}
-
-	return new Promise((resolve, reject) => {
-		fs.readFile(file, (error, content) => {
-			if (error) {
-				return reject(error);
-			}
-
-			const compressedContent = brotli.compressSync(content, config);
-
-			if (compressedContent !== null && stat.size > compressedContent.length) {
-				fs.writeFile(`${file}.br`, compressedContent, () => resolve());
-			} else {
-				resolve();
-			}
-		});
-	});
 }
