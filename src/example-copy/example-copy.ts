fdf0a9e3059fc7310ddda56b26a7175987e16038
Ivan Solovev <ivan@solovev.one>
2020-02-16 20:53:41 +0300

    Add feedback when copying styles

--- src/example-copy/example-copy.ts
+++ src/example-copy/example-copy.ts
@@ -3,11 +3,26 @@ import { getElement, getElementsList } from "../helpers/getElement";
 import { forNodeList } from "../helpers/forNodeList";
 import { copyTextFromElement } from "../helpers/copyText";
 
+const selectorIconAction = ".example-copy__icon-action";
+const selectorIconDone = ".example-copy__icon-done";
+const classIconHidden = "example-copy__icon-hidden";
+
 const list = getElementsList(selectorCopyButton);
 
 forNodeList(list, (item) => {
 	item.addEventListener("click", () => {
 		const code = getElement(selectorCopyCode, item.parentElement);
 		copyTextFromElement(code);
+
+		const iconAction = getElement(selectorIconAction, item);
+		const iconDone = getElement(selectorIconDone, item);
+
+		iconAction.classList.add(classIconHidden);
+		iconDone.classList.remove(classIconHidden);
+
+		setTimeout(() => {
+			iconAction.classList.remove(classIconHidden);
+			iconDone.classList.add(classIconHidden);
+		}, 1500);
 	});
 });
