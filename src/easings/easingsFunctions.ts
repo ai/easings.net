2f5dd25c959d276cb9b89086db775be7548f2f77
AlexTaietti <taiettialex@yahoo.it>
2021-03-03 00:59:28 +0000

    add EasingFunction type and EasingDictionary interface

--- src/easings/easingsFunctions.ts
+++ src/easings/easingsFunctions.ts
@@ -1,3 +1,9 @@
+type EasingFunction = (progress: number) => number;
+
+interface EasingDictionary {
+	[easing: string]: EasingFunction;
+};
+
 const pow = Math.pow;
 const sqrt = Math.sqrt;
 const sin = Math.sin;
@@ -9,7 +15,7 @@ const c3 = c1 + 1;
 const c4 = (2 * PI) / 3;
 const c5 = (2 * PI) / 4.5;
 
-function bounceOut(x: number): number {
+let bounceOut: EasingFunction = function (x) {
 	const n1 = 7.5625;
 	const d1 = 2.75;
 
@@ -24,117 +30,118 @@ function bounceOut(x: number): number {
 	}
 }
 
-const easingsFunctions: any = {
-	easeInQuad(x: number) {
+const easingsFunctions: EasingDictionary = {
+	linear: (x) => x,
+	easeInQuad: function (x) {
 		return x * x;
 	},
-	easeOutQuad(x: number) {
+	easeOutQuad: function (x) {
 		return 1 - (1 - x) * (1 - x);
 	},
-	easeInOutQuad(x: number) {
+	easeInOutQuad: function (x) {
 		return x < 0.5 ? 2 * x * x : 1 - pow(-2 * x + 2, 2) / 2;
 	},
-	easeInCubic(x: number) {
+	easeInCubic: function (x) {
 		return x * x * x;
 	},
-	easeOutCubic(x: number) {
+	easeOutCubic: function (x) {
 		return 1 - pow(1 - x, 3);
 	},
-	easeInOutCubic(x: number) {
+	easeInOutCubic: function (x) {
 		return x < 0.5 ? 4 * x * x * x : 1 - pow(-2 * x + 2, 3) / 2;
 	},
-	easeInQuart(x: number) {
+	easeInQuart: function (x) {
 		return x * x * x * x;
 	},
-	easeOutQuart(x: number) {
+	easeOutQuart: function (x) {
 		return 1 - pow(1 - x, 4);
 	},
-	easeInOutQuart(x: number) {
+	easeInOutQuart: function (x) {
 		return x < 0.5 ? 8 * x * x * x * x : 1 - pow(-2 * x + 2, 4) / 2;
 	},
-	easeInQuint(x: number) {
+	easeInQuint: function (x) {
 		return x * x * x * x * x;
 	},
-	easeOutQuint(x: number) {
+	easeOutQuint: function (x) {
 		return 1 - pow(1 - x, 5);
 	},
-	easeInOutQuint(x: number) {
+	easeInOutQuint: function (x) {
 		return x < 0.5 ? 16 * x * x * x * x * x : 1 - pow(-2 * x + 2, 5) / 2;
 	},
-	easeInSine(x: number) {
+	easeInSine: function (x) {
 		return 1 - cos((x * PI) / 2);
 	},
-	easeOutSine(x: number) {
+	easeOutSine: function (x) {
 		return sin((x * PI) / 2);
 	},
-	easeInOutSine(x: number) {
+	easeInOutSine: function (x) {
 		return -(cos(PI * x) - 1) / 2;
 	},
-	easeInExpo(x: number) {
+	easeInExpo: function (x) {
 		return x === 0 ? 0 : pow(2, 10 * x - 10);
 	},
-	easeOutExpo(x: number) {
+	easeOutExpo: function (x) {
 		return x === 1 ? 1 : 1 - pow(2, -10 * x);
 	},
-	easeInOutExpo(x: number) {
+	easeInOutExpo: function (x) {
 		return x === 0
 			? 0
 			: x === 1
-			? 1
-			: x < 0.5
-			? pow(2, 20 * x - 10) / 2
-			: (2 - pow(2, -20 * x + 10)) / 2;
+				? 1
+				: x < 0.5
+					? pow(2, 20 * x - 10) / 2
+					: (2 - pow(2, -20 * x + 10)) / 2;
 	},
-	easeInCirc(x: number) {
+	easeInCirc: function (x) {
 		return 1 - sqrt(1 - pow(x, 2));
 	},
-	easeOutCirc(x: number) {
+	easeOutCirc: function (x) {
 		return sqrt(1 - pow(x - 1, 2));
 	},
-	easeInOutCirc(x: number) {
+	easeInOutCirc: function (x) {
 		return x < 0.5
 			? (1 - sqrt(1 - pow(2 * x, 2))) / 2
 			: (sqrt(1 - pow(-2 * x + 2, 2)) + 1) / 2;
 	},
-	easeInBack(x: number) {
+	easeInBack: function (x) {
 		return c3 * x * x * x - c1 * x * x;
 	},
-	easeOutBack(x: number) {
+	easeOutBack: function (x) {
 		return 1 + c3 * pow(x - 1, 3) + c1 * pow(x - 1, 2);
 	},
-	easeInOutBack(x: number) {
+	easeInOutBack: function (x) {
 		return x < 0.5
 			? (pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
 			: (pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
 	},
-	easeInElastic(x: number) {
+	easeInElastic: function (x) {
 		return x === 0
 			? 0
 			: x === 1
-			? 1
-			: -pow(2, 10 * x - 10) * sin((x * 10 - 10.75) * c4);
+				? 1
+				: -pow(2, 10 * x - 10) * sin((x * 10 - 10.75) * c4);
 	},
-	easeOutElastic(x: number) {
+	easeOutElastic: function (x) {
 		return x === 0
 			? 0
 			: x === 1
-			? 1
-			: pow(2, -10 * x) * sin((x * 10 - 0.75) * c4) + 1;
+				? 1
+				: pow(2, -10 * x) * sin((x * 10 - 0.75) * c4) + 1;
 	},
-	easeInOutElastic(x: number) {
+	easeInOutElastic: function (x) {
 		return x === 0
 			? 0
 			: x === 1
-			? 1
-			: x < 0.5
-			? -(pow(2, 20 * x - 10) * sin((20 * x - 11.125) * c5)) / 2
-			: (pow(2, -20 * x + 10) * sin((20 * x - 11.125) * c5)) / 2 + 1;
+				? 1
+				: x < 0.5
+					? -(pow(2, 20 * x - 10) * sin((20 * x - 11.125) * c5)) / 2
+					: (pow(2, -20 * x + 10) * sin((20 * x - 11.125) * c5)) / 2 + 1;
 	},
-	easeInBounce(x: number) {
+	easeInBounce: function (x) {
 		return 1 - bounceOut(1 - x);
 	},
 	easeOutBounce: bounceOut,
-	easeInOutBounce(x: number) {
+	easeInOutBounce: function (x) {
 		return x < 0.5
 			? (1 - bounceOut(1 - 2 * x)) / 2
 			: (1 + bounceOut(2 * x - 1)) / 2;
